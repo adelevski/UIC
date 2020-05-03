6924b6e2e35a7391ac6270ddeb0329062434464a
adelevski@gmail.com <adelevski@gmail.com>
2020-05-03 00:47:39 -0500

    starting a bip mini-project

--- BIP/Simple PCA Project/pca.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-from sklearn.decomposition import PCA
-from sklearn.preprocessing import StandardScaler
-
-data = pd.read_csv('breast-cancer.csv')
-df = pd.DataFrame(data)
-
-
